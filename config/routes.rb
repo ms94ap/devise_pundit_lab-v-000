f9f17138bcbee7989d3d112d3cc17ff3f5909e7b
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-06 14:33:52 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'notes#index'
 
+  
+
+  # get 'users' => 'user#index'
+
+  resources :users
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
